b9bc60f3913cf3ee1c08abe909673f98cc0aef77
alex <madfoxsec@protonmail.com>
2024-02-03 16:45:46 +0100

    Add functionality to stop and resume the timer

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
 let timer = document.querySelector('#timer');
 let startButton = document.querySelector('#start');
-let stopButton = document.querySelector('#stop')
-let resumeButton = document.querySelector('#resume')
+let stopButton = document.querySelector('#stop');
+let resetButton = document.querySelector('#reset');
 const MILLISECONDS_PER_MINUTE = 60000;
 let pomodoroTime = 25;
-let intervallID;
-let startTime = new Date();
-let savedDifference;
-let difference;
+let intervalID;
+let endTime;
+let savedDifference = null;
+let difference = null; 
+let timerIsRunning = false;
 
 startButton.addEventListener('click', ()=>{
-    let endTime = new Date(startTime.getTime() + (pomodoroTime * MILLISECONDS_PER_MINUTE))
-    difference = getDateDifferenceInSeconds(startTime, endTime);
-    intervallID = setInterval(()=>{
-        timer.innerHTML = Math.floor(difference/60) + ':' + (difference % 60);
-        difference--;
-        if (difference <= 0) {
-            clearInterval(intervalID); 
-            timer.innerHTML = 'Time for a break.';
+    if (!timerIsRunning){
+        timerIsRunning = true;
+        let startTime = new Date();
+        if (savedDifference != null){
+            endTime = new Date(startTime.getTime() + (savedDifference * MILLISECONDS_PER_MINUTE))
+            difference = savedDifference;
+            savedDifference = null;
         }
-    }, 1000);
-}, { once: true });
+        else{  
+            endTime = new Date(startTime.getTime() + (pomodoroTime * MILLISECONDS_PER_MINUTE))
+            difference = getDateDifferenceInSeconds(startTime, endTime);
+        }
+        
+        intervalID = setInterval(()=>{
+            --difference; //this avoids displaying 25:00 twice
+            let minutes = Math.floor(difference / 60);
+            let seconds = difference % 60;
+            timer.innerHTML = minutes + ':' + (seconds < 10 ? '0' + seconds : seconds);
+            if (difference <= 0) {
+                clearInterval(intervalID); 
+                timer.innerHTML = 'Time for a break.';
+                timerIsRunning = false;
+            }
+        }, 1000);
+    }
+});
 
 stopButton.addEventListener('click', ()=>{
     savedDifference = difference;
-    clearInterval(intervallID);
+    clearInterval(intervalID);
+    timerIsRunning = false;
 });
+
+
 
 
 
